4d9a60964f4735ed1cc3015dab2ea7a8e67a8a7b
vishymails <vishymails@gmail.com>
2023-01-25 18:31:15 +0530

    added

---
+++ ANGULAR15/example59/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'example59';
+
+  
+form = new FormGroup({
+  name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+  email: new FormControl('', [Validators.required, Validators.email]),
+  body: new FormControl('', Validators.required)
+});
+ 
+get f(){
+  return this.form.controls;
+}
+ 
+submit(){
+  console.log(this.form.value);
+}
+}
